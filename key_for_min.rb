5957601f048d31078834184fa79efc2a9c00b36f
Myles <mylesjanik@gmail.com>
2020-11-01 16:38:54 -0800

    commit tree 5e4f19f19ee4a8721feb48481760154c53bf1ba1

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+    temp_number = 4611686018427387903
+    temp_string = ""
+    name_hash.each do |key, value|
+        if value < temp_number
+            temp_number = value
+            temp_string = key
+        end
+    end
+
+    if temp_string == ""
+        return nil
+    else
+        return temp_string
+    end
 
 end
